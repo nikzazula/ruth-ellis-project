b3eea4caa0cb8f6dbc1dff74cceaaa41f08b79a1
nzazula <nzazula@umich.edu>
2021-12-06 01:50:40 -0500

    yea

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,3 +5,8 @@
 $('#contact-flip-card #contact-flip-card-inner').click(function(){
     $(this).toggleClass("flip");
 });
+var frmvalidator  = new Validator("contactform");
+    frmvalidator.addValidation("name","req","Please provide your name");
+    frmvalidator.addValidation("email","req","Please provide your email");
+    frmvalidator.addValidation("email","email",
+      "Please enter a valid email address");
